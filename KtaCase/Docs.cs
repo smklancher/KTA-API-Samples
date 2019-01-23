8087f6c7a3ecc09b16a357e8325d53cd39189a4e
Stephen Klancher <smknight@gmail.com>
2019-01-22 17:57:39 -0800

    more doc details, including tiff compression

--- KtaCase/Docs.cs
+++ KtaCase/Docs.cs
@@ -1,18 +1,16 @@
-﻿using System;
+﻿using Agility.Sdk.Model.Capture;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using TotalAgility.Sdk;
-using Agility.Sdk.Model;
-using Agility.Sdk.Model.Capture;
-using System.Diagnostics;
 
 namespace KtaCase
 {
     public class Docs
     {
-
         public string Details(string sessionId, string documentId)
         {
             var s = new StringBuilder($"Id: {documentId}");
@@ -23,11 +21,44 @@ namespace KtaCase
             s.AppendLine(ReflectionHelper.PropertyList(d));
             s.AppendLine();
 
+            string compressionName = string.Empty;
+
+            try
+            {
+                using (var tiffstream = ds.GetDocumentFile(sessionId, null, documentId, "tiff"))
+                using (Image img = Image.FromStream(tiffstream))
+                {
+                    compressionName = ImageData.GetCompressionName(img);
+                }
+            }
+            catch (Exception ex)
+            {
+                compressionName = $"Error: {ex.Message}";
+            }
+            s.AppendLine("GetDocumentFile TIFF Compression: " + compressionName);
+
+            try
+            {
+                string filename = ds.GetDocumentAsFile(sessionId, documentId, System.IO.Path.GetTempPath(), Guid.NewGuid().ToString() + ".tif");
+                using (Image img = Image.FromFile(filename))
+                {
+                    compressionName = ImageData.GetCompressionName(img);
+                }
+                System.IO.File.Delete(filename);
+            }
+            catch (Exception ex)
+            {
+                compressionName = $"Error: {ex.Message}";
+            }
+            s.AppendLine("GetDocumentAsFile TIFF Compression: " + compressionName);
+
+            s.AppendLine();
+
             if (d.Fields != null)
             {
                 foreach (var f in d.Fields)
                 {
-                    s.AppendLine(ReflectionHelper.PropertyList(f));
+                    s.AppendLine(DocField(sessionId, d, f));
                 }
             }
 
@@ -35,7 +66,7 @@ namespace KtaCase
             s.AppendLine($"Pages: {d.NumberOfPages}");
             s.AppendLine();
             int pageIndex = 0;
-            foreach(var p in d.Pages)
+            foreach (var p in d.Pages)
             {
                 s.AppendLine($"Page {pageIndex + 1} of {d.NumberOfPages}");
                 s.AppendLine(ReflectionHelper.PropertyList(p));
@@ -76,23 +107,64 @@ namespace KtaCase
 
                 //var pageProperties = ds.GetPagePropertyValues(sessionId, documentId, ppic);
 
-                WordCollection words=DocPageWords(sessionId,documentId,pageIndex);
+                WordCollection words = DocPageWords(sessionId, documentId, pageIndex);
+
                 //foreach(var pageProps in pageProperties)
                 //{
                 //    words=(WordCollection)pageProps.PropertyCollection.Where(x => x.SystemFieldIdentity.Name == "Words").FirstOrDefault()?.Value;
-                    if (words != null)
+                if (words != null)
+                {
+                    s.AppendLine("GetPagePropertyValues-Words property");
+                    foreach (var w in words)
                     {
-                        s.AppendLine("GetPagePropertyValues-Words property");
-                        foreach (var w in words)
-                        {
-                            s.AppendLine($"{w.Text}");
-                        }
+                        s.AppendLine($"{w.Text}");
                     }
+                }
+
                 //}
 
                 pageIndex++;
             }
-            
+
+            return s.ToString();
+        }
+
+        public string DocField(string sessionId, Document d, RuntimeFieldData f)
+        {
+            var s = new StringBuilder($"Field: {f.Name}");
+            var ds = new CaptureDocumentService();
+
+            s.AppendLine();
+            s.AppendLine(ReflectionHelper.PropertyList(f));
+            s.AppendLine();
+
+            var fpic = new FieldPropertiesIdentityCollection()
+            {
+                new FieldPropertiesIdentity()
+                {
+                    Identity=new RuntimeFieldIdentity(f.Id),
+                    FieldSystemProperties=new FieldSystemPropertyIdentityCollection()
+                    {
+                        new FieldSystemPropertyIdentity()
+                        {
+                            Name="OriginalText",
+                            Id="E52E91102A6B4052A4B35CD71E4B0016"
+                        }
+                    }
+                }
+            };
+
+            var fpc = ds.GetDocumentFieldPropertyValues(sessionId, null, d.Id, fpic);
+
+            // for each field returned (only one here)
+            foreach (var fp in fpc)
+            {
+                // for each property
+                foreach (var fsp in fp.PropertyCollection)
+                {
+                    s.AppendLine($"{fsp.SystemFieldIdentity.Name} = {fsp.Value}");
+                }
+            }
 
             return s.ToString();
         }
@@ -125,6 +197,7 @@ namespace KtaCase
             // Build the request for specific properties from specific pages
             // See available properties: https://docshield.kofax.com/KTA/en_US/750-4kcae04o43/help/API/latest/class_agility_1_1_sdk_1_1_services_1_1_capture_document_service.html#a371ac5b01657cab2d46623c7f43f84b4
             var ppic = new PagePropertiesIdentityCollection(){
+
                 // Could request properties from more than one page here if needed
                 new PagePropertiesIdentity(p.Id,  new FieldSystemPropertyIdentityCollection() {
                     new FieldSystemPropertyIdentity() { Name = "Words" }
@@ -145,5 +218,20 @@ namespace KtaCase
             return words;
         }
 
+        private FieldSystemPropertyIdentityCollection AllFieldSystemProperties()
+        {
+            var x = new FieldSystemPropertyIdentityCollection();
+
+            foreach (var fsp in Agility.Server.Core.Model.Constants.CaptureConfigurationConstants.FieldProperties)
+            {
+                x.Add(new FieldSystemPropertyIdentity()
+                {
+                    Name = fsp.Key,
+                    Id = fsp.Value
+                });
+            }
+
+            return x;
+        }
     }
 }
---
+++ KtaCase/ImageData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace KtaCase
+{
+    public class ImageData
+    {
+        /// <summary>
+        /// https://www.awaresystems.be/imaging/tiff/tifftags/compression.html
+        /// </summary>
+        public enum TiffCompression
+        {
+            NONE = 1,
+            CCITTRLE = 2,
+            CCITTFAX3 = 3,
+            CCITTFAX4 = 4,
+            LZW = 5,
+            OJPEG = 6,
+            JPEG = 7,
+            ADOBE_DEFLATE = 8,
+            NEXT = 32766,
+            CCITTRLEW = 32771,
+            PACKBITS = 32773,
+            THUNDERSCAN = 32809,
+            IT8CTPAD = 32895,
+            IT8LW = 32896,
+            IT8MP = 32897,
+            IT8BL = 32898,
+            PIXARFILM = 32908,
+            PIXARLOG = 32909,
+            DEFLATE = 32946,
+            DCS = 32947,
+            JBIG = 34661,
+            SGILOG = 34676,
+            SGILOG24 = 34677,
+            JP2000 = 34712
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/1561112/221018
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static int GetCompressionType(Image image)
+        {
+            int compressionTagIndex = Array.IndexOf(image.PropertyIdList, 0x103);
+            PropertyItem compressionTag = image.PropertyItems[compressionTagIndex];
+            return BitConverter.ToInt16(compressionTag.Value, 0);
+        }
+
+        public static string GetCompressionName(Image image)
+        {
+            int comptype = GetCompressionType(image);
+            return Enum.GetName(typeof(TiffCompression), comptype);
+        }
+    }
+}
